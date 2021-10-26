54c33419afca0d24c6a2194506344cffb2d06f62
morfeo <morfeo@DESKTOP-MTCF4P8>
2021-10-26 13:43:44 +0200

    same as before

---
+++ draft stuff/clases extra/FriendNode.java
@@ -0,0 +1,62 @@
+package network;
+
+public class FriendNode {
+	
+	private String film;
+	private String users;
+	private FriendNode firstChild, siblings;
+	
+	
+	public FriendNode(){
+		this.film = "";
+		this.users = "";
+		firstChild = null;
+		siblings = null;
+	}
+	
+	public FriendNode(String film){
+		this.film = film;
+		this.users = "";
+		firstChild = null;
+		siblings = null;
+	}
+	
+	public FriendNode(String profile, String user){
+		this.film = profile;
+		this.users = user;
+		firstChild = null;
+		siblings = null;
+	}
+	
+	public FriendNode getFirstChild(){
+		return this.firstChild;
+	}
+	
+	public FriendNode getSiblings(){
+		return this.siblings;
+	}
+	
+	public void setFirstChild(FriendNode node) {
+		this.firstChild = node;
+	}
+	
+	public void setSiblings(FriendNode node) {
+		this.siblings = node;
+	}
+	
+	public String getFilm() {
+		return film;
+	}
+	
+	public void setFilm(String info) {
+		this.film=info;
+	}
+	
+	public String getUsers() {
+		return users;
+	}
+	
+	public void setUsers(String user) {
+		this.users = this.users + user;
+	}
+}
