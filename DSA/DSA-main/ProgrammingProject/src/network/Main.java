19bf41513b3d470d6be98037ed43d49f83c34711
morfeo <morfeo@DESKTOP-MTCF4P8>
2021-11-09 09:27:33 +0100

    Fixed main, removed unnecessary 99 while thing. Fixed reimporting (you forgot to reset the boolean)

--- DSA/DSA-main/ProgrammingProject/src/network/Main.java
+++ DSA/DSA-main/ProgrammingProject/src/network/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {  
-			Network Network = new Network();
-			
+			Network net = new Network();
+
 			System.out.println("=-=-=-=-=-=");
 			System.out.println(" MAIN MENU:");
 			System.out.println("1. Import people into network");
@@ -22,71 +22,63 @@ public class Main {
 
 
 			System.out.print("Select your option > ");
-			int number=99;//random value to start from for the next try/catch
-			boolean done=false;
-			Scanner keyboard = new Scanner(System.in);
-			while(number==99) {
-			try
-			{
-			number = keyboard.nextInt();
-			}
-			catch (Exception e) {
-	            System.out.print("That is not a number, type a number:");
-	           keyboard.next();// Move to next other wise exception
-	        }
-			}
+
+
+			boolean executed = false;
+			Scanner console = new Scanner(System.in);
+			int number = console.nextInt();
+
 			String path;
 			while (number != 0) {
-			
+
 				switch(number) {
-				case 1:
+				
+				case 1:													// import people into network
 					System.out.print("\nChoose file to import... > ");
-					while(!done) {
-							try
-							{
-								path = keyboard.next();
-								Network.importUsers(path);
-								System.out.println("People imported successfully");
-								done=true;
-							}
-							catch(FileNotFoundException e)
-							{
-								System.out.print("Incorrect file name, introduce a valid file name (don't forget to add .txt):");
-							}
-							catch(ArrayIndexOutOfBoundsException e)
-							{
-								System.out.print("The contents of the file are incorrect, correct them and try again, or try with another file:");
-							}
+					executed = false;
+					while(!executed) {
+						try {
+							path = console.next();
+							net.importUsers(path);
+							System.out.println("People imported successfully.\n");
+							executed = true;
 							
-								}
+						} catch(FileNotFoundException e) {
+							System.out.print("Incorrect file name, introduce a valid file name (don't forget to add .txt) >");
+							
+						} catch(ArrayIndexOutOfBoundsException e) {
+							System.out.print("The contents of the file are incorrect, correct them and try again, or try with another file > ");
+						}
+
+					}
 					break;
-				case 2:
+					
+				case 2:														// import relationships into network
 					System.out.print("\nChoose file to import... > ");
-					done=false;
-					while(!done) {
-					try
-					{
-						path = keyboard.next();
-						Network.importRelationships(path);
-						System.out.println("Relationships imported successfully"); //TODO it can load a friends file too, should we prevent that?
-						done=true;
+					executed = false;
+					while(!executed) {
+						try {
+							path = console.next();
+							net.importRelationships(path);
+							System.out.println("Relationships imported successfully"); //TODO it can load a friends file too, should we prevent that?
+							executed=true;
+							
+						} catch(FileNotFoundException e) {
+							System.out.println("Incorrect file name");
+							
+						} catch(ArrayIndexOutOfBoundsException e) {
+							System.out.print("The contents of the file are incorrect, correct them and try again, or try with another file:");
+						}
 					}
-					catch(FileNotFoundException e)
-					{
-						System.out.println("Incorrect file name");
-					}
-					catch(ArrayIndexOutOfBoundsException e)
-					{
-						System.out.print("The contents of the file are incorrect, correct them and try again, or try with another file:");
-					}
-								}
 					break;
+					
 				case 3:
 					System.out.print("\nChoose file to export to... > ");
-					path = keyboard.next();
-					Network.exportUserdata(path);
+					path = console.next();
+					net.exportUserdata(path);
 					System.out.println("\nData successfully saved in " + path);
 					break;
+					
 				case 4:
 					System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 					System.out.println(" SEARCH AND UPDATE INFORMATION OF USERS: ");
@@ -96,55 +88,55 @@ public class Main {
 					System.out.println("4. Filter by residents matched in residential.txt");
 					System.out.println("5. Filter by identical film classes");
 					System.out.print("Select your option > ");
-					int search = keyboard.nextInt();
+					int search = console.nextInt();
 					switch(search) {
 					case 1 :
 						// 6
 						System.out.println("\nEnter lastname: ");
-						String lastname = keyboard.next();
+						String lastname = console.next();
 						System.out.println("\nSelect printing method: ");
 						System.out.println("0. Export to text file...");
 						System.out.println("1. Print to console");
 						System.out.print("> ");
 
 
-						int select = keyboard.nextInt();
-						Network.exportRelationships(lastname, select);
+						int select = console.nextInt();
+						net.exportRelationships(lastname, select);
 						break;
 					case 2 :
 						//7
 						System.out.print("\nEnter city > ");
-						String city = keyboard.next();
-						Network.printByCity(city);
+						String city = console.next();
+						net.printByCity(city);
 						break;
 					case 3 :
 						//8
 						System.out.println("\nEnter year of time frame beginning > ");
-						int date1 = keyboard.nextInt();
+						int date1 = console.nextInt();
 						System.out.print("Enter year of time frame end > ");
 
-						int date2 = keyboard.nextInt();
-						Network.printbyTimeFrame(date1, date2);
+						int date2 = console.nextInt();
+						net.printbyTimeFrame(date1, date2);
 						break;
 					case 4 :
 						//9
-						Network.loadResidential();
+						net.loadResidential();
 						break;
 					case 5 :
 						//10
-						Network.printFilmClasses();
+						net.printFilmClasses();
 						break;
 					default:
-						
+
 					}
 					break;
-					
-				// case 5 not done yet
-					
+
+					// case 5 not done yet
+
 				default:	
-					
+
 				}
-				
+
 				System.out.println("\n\n=-=-=-=-=-=");
 				System.out.println(" MAIN MENU:");
 				System.out.println("1. Import people into network");
@@ -156,9 +148,11 @@ public class Main {
 
 
 				System.out.print("Select your option > ");
-				number = keyboard.nextInt();
+				number = console.nextInt();
 			}
-			keyboard.close();
+
+			console.close();
+
 		} catch (IOException e)  {  
 			e.printStackTrace();  
 		}
